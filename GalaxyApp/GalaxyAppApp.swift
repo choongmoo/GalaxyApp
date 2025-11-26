1fa5b08566fb40c2d3fc4c6e84fd807f8905219c
choongmoo <wdokl88@naver.com>
2025-11-26 15:03:25 +0900

    Initial Commit

---
+++ GalaxyApp/GalaxyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyAppApp.swift
+//  GalaxyApp
+//
+//  Created by Student on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct GalaxyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GalaxyAppTests/GalaxyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyAppTests.swift
+//  GalaxyAppTests
+//
+//  Created by Student on 11/26/25.
+//
+
+import Testing
+@testable import GalaxyApp
+
+struct GalaxyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
